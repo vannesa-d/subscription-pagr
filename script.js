db22e1144215dda0f2f0a9f2edf3a402967002eb
vannesa-d <vannesa.dev@gmail.com>
2024-12-05 15:20:20 +0200

    added js file

---
+++ script.js
@@ -0,0 +1,5 @@
+function subscribe(plan) {
+    alert(`You have selected the ${plan}. Proceed to payment.`);
+    // Redirect to a payment page
+    window.location.href = "/payment"; // Replace with your payment page link
+}
